4a89c8ee2d0e076a8ee6f0c69a026ea04439d034
Christine Nyambwari <159689583+stine-ri@users.noreply.github.com>
2024-11-25 13:03:56 -0800

    Update NavBar.tsx

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(prevState => !prevState);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles['navbar-logo']}>
         <h1>Stine Rentals</h1>
       </div>
-      <ul className={styles['navbar-links']}>
+      <button className={styles['hamburger-menu']} onClick={toggleMenu}>
+        &#9776; {/* Hamburger icon */}
+      </button>
+      <ul className={`${styles['navbar-links']} ${isMenuOpen ? styles['open'] : ''}`}>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/about-us">About us</Link></li>
         <li><Link to="/contact-us">Contact us</Link></li>
